21006050b8cc719b9e2c9f0cdb351d59c4070203
Joaquim Perez <jivkim@gmail.com>
2025-02-11 15:28:49 +0100

    add Building component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
+import Building from "./svg/Building.vue";
 </script>
 
 <template>
   <div>
-    Guesthouse
+    <Building class="building"></Building>
   </div>
 </template>
 
 <style scoped>
+.building {
+  max-width: 800px;
+  display: block;
+  margin: auto;
+}
 </style>
