e19eadbe19e784d0653beb7a59ba0bfe794c2bea
Joaquim Perez <jivkim@gmail.com>
2025-02-11 17:00:47 +0100

    refactor: move components in state.js

--- src/state.js
+++ src/state.js
@@ -1,21 +1,31 @@
-import { reactive, watch } from "vue";
+import { markRaw, reactive, watch } from "vue";
+import Ground from "./svg/floors/0_Ground.vue";
+import First from "./svg/floors/1_First.vue";
+import Second from "./svg/floors/2_Second.vue";
+import Third from "./svg/floors/3_Third.vue";
+import Roof from "./svg/floors/4_Roof.vue";
 
 const state = reactive({
   floors: [
     {
       style: {},
+      component: markRaw(Ground),
     },
     {
       style: {},
+      component: markRaw(First),
     },
     {
       style: {},
+      component: markRaw(Second),
     },
     {
       style: {},
+      component: markRaw(Third),
     },
     {
       style: {},
+      component: markRaw(Roof),
     },
   ],
   openFloor: null,
--- src/svg/Building.vue
+++ src/svg/Building.vue
@@ -1,20 +1,15 @@
 <script setup>
-import Ground from './floors/0_Ground.vue';
-import First from './floors/1_First.vue'
-import Second from './floors/2_Second.vue'
-import Third from './floors/3_Third.vue'
-import Roof from './floors/4_Roof.vue'
-
 import state from '../state';
 </script>
 
 <template>
     <svg viewBox="0 0 2000 2000">
-        <Ground :style="state.floors[0].style" @click="state.openFloor = 0"/>
-        <First :style="state.floors[1].style" @click="state.openFloor = 1"/>
-        <Second :style="state.floors[2].style" @click="state.openFloor = 2"/>
-        <Third :style="state.floors[3].style" @click="state.openFloor = 3"/>
-        <Roof :style="state.floors[4].style" @click="state.openFloor = 4"/>
+        <component
+            v-for="(floor, index) in state.floors"
+            :is="floor.component"
+            :style="floor.style"
+            @click="state.openFloor = index"
+        />
     </svg>
 </template>
 
