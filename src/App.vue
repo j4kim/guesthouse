48e77bd3eb164d699ee99bbedeaa262562c67656
Joaquim Perez <jivkim@gmail.com>
2025-02-12 22:31:46 +0100

    button to quit room

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { openFloor } from "./store";
+import { openFloor, room } from "./store";
 import Building from "./svg/Building.vue";
 </script>
 
 <template>
     <div>
         <Building class="building"></Building>
-        <div v-if="openFloor !== null" class="controls">
+        <div v-if="room !== null" class="controls">
+            <button @click="room = null">⤴️</button>
+        </div>
+        <div v-else-if="openFloor !== null" class="controls">
             <button @click="openFloor = null">🏠</button>
             <button @click="openFloor--" :disabled="openFloor === 0">⬇️</button>
             <button @click="openFloor++" :disabled="openFloor === 4">⬆️</button>
