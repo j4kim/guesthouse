413fd4d12f6fbd8d109af43706d6df865866402c
Joaquim Perez <jivkim@gmail.com>
2025-02-12 20:29:51 +0100

    remove first floor walls when selected

--- src/svg/floors/1_First.vue
+++ src/svg/floors/1_First.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { openFloor } from "../../store";
 import FirstInside from "./1_FirstInside.vue"
 import FirstOutside from "./1_FirstOutside.vue";
 </script>
 
 <template>
     <g id="first_floor">
-        <FirstInside></FirstInside>
-        <FirstOutside></FirstOutside>
+        <FirstInside />
+        <FirstOutside :style="{
+            transition: 'opacity 1s',
+            opacity: openFloor === 1 ? 0 : 1,
+        }" />
     </g>
 </template>
