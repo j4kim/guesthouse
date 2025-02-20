d27c0ef3d2fc35530f451b5359c5392b821cd949
Joaquim Perez <jivkim@gmail.com>
2025-02-20 23:03:21 +0100

    update roof svg

--- src/svg/floors/4_Roof.vue
+++ src/svg/floors/4_Roof.vue
@@ -1,74 +1,221 @@
 <template>
     <g id="roof">
-        <g id="roof_floor">
-            <polygon
-                points="358.18 570 920.1 245.58 1637.9 660 1075.98 984.42 358.18 570"
-                fill="#fff"
-            />
-            <path
-                d="M920.1,246.15l716.8,413.85-560.92,323.85-716.8-413.85L920.1,246.15M920.1,245l-562.92,325,718.8,415,562.92-325L920.1,245h0Z"
-            />
-        </g>
-        <g id="roof_guides">
-            <line
-                x1="1201.55"
-                y1="332.5"
-                x2="638.64"
-                y2="7.5"
-                fill="#fff"
-                stroke="#000"
-                stroke-miterlimit="10"
-            />
-            <line
-                x1="638.64"
-                y1="407.5"
-                x2="1357.44"
-                y2="822.5"
-                fill="#fff"
-                stroke="#000"
-                stroke-miterlimit="10"
-            />
-            <line
-                x1="638.64"
-                y1="7.5"
-                x2="638.64"
-                y2="407.5"
-                fill="#fff"
-                stroke="#000"
-                stroke-miterlimit="10"
-            />
-        </g>
-        <g id="roof_east">
-            <polygon
-                points="638.8 8.29 919.23 244.92 358.37 568.73 638.8 8.29"
-                fill="#fff"
-            />
-            <path
-                d="M638.96,9.08l279.4,235.76-558.8,322.62L638.96,9.08M638.64,7.5l-281.46,562.5L920.1,245,638.64,7.5h0Z"
-            />
-        </g>
         <polygon
-            id="roof_south"
-            points="920.1 245 1638.9 660 1201.55 332.5 638.64 7.5 920.1 245"
+            id="roof_floor"
+            points="1076 985 1639 660 920.1 245 357.2 570 1076 985"
             fill="#fff"
             stroke="#000"
             stroke-miterlimit="10"
         />
-        <g id="roof_west">
+        <g>
+            <g>
+                <polygon
+                    points="340.5 589.8 638.8 8.2 1201 332.8 1075.6 1014.2 340.5 589.8"
+                    fill="#fff"
+                    stroke-width="0"
+                />
+                <path
+                    d="m639,8.9l561.4,324.1-125.3,680.4-734-423.9L639,8.9m-.4-1.4l-298.8,582.5,736.1,425,125.6-682.5L638.6,7.5h0Z"
+                    stroke-width="0"
+                />
+            </g>
             <polygon
-                points="1201.89 333.38 1637.99 659.95 1076.68 984.02 1201.89 333.38"
+                points="400.3 595 920.1 895 920.1 755 400.1 454.8 400.3 595"
                 fill="#fff"
+                stroke="#000"
+                stroke-miterlimit="10"
             />
-            <path
-                d="M1202.23,334.26l434.85,325.63-559.71,323.15,124.86-648.78M1201.55,332.5l-125.57,652.5,562.92-325-437.34-327.5h0Z"
+            <g id="windows_north">
+                <polygon
+                    points="872.8 827.4 829.3 802 829.3 722.3 872.8 747.6 872.8 827.4"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="777.5 772.4 734 747 734 667.3 777.5 692.6 777.5 772.4"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="682.2 717.3 638.7 692 638.7 612.2 682.2 637.6 682.2 717.3"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="586.8 662.3 543.4 637 543.4 557.2 586.8 582.6 586.8 662.3"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="491.5 607.3 448 581.9 448 502.2 491.5 527.5 491.5 607.3"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="872.6 827 894.2 839.5 894.2 759.8 872.6 747.3 872.6 827"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="807.6 789.5 829.3 802 829.3 722.3 807.6 709.8 807.6 789.5"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="777.2 771.8 798.8 784.3 798.8 704.6 777.2 692.1 777.2 771.8"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="712.2 734.3 733.9 746.8 733.9 667.1 712.2 654.6 712.2 734.3"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="681.8 716.6 703.4 729.1 703.4 649.4 681.8 636.9 681.8 716.6"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="616.8 679.1 638.5 691.6 638.5 611.9 616.8 599.4 616.8 679.1"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="586.4 661.4 608 673.9 608 594.2 586.4 581.7 586.4 661.4"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="521.4 623.9 543.1 636.4 543.1 556.7 521.4 544.2 521.4 623.9"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="491 606.2 512.6 618.7 512.6 539 491 526.5 491 606.2"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="426 568.7 447.7 581.2 447.7 501.5 426 489 426 568.7"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+            </g>
+            <polygon
+                points="509.3 377.8 1028.8 678 920.1 755 400.1 454.8 509.3 377.8"
+                fill="#fff"
+                stroke="#000"
+                stroke-miterlimit="10"
+            />
+            <polygon
+                points="1028.8 678 920.1 895 920.1 755 1028.8 678"
+                fill="#fff"
+                stroke="#000"
+                stroke-miterlimit="10"
             />
         </g>
-        <polygon
-            id="roof_north"
-            points="357.18 570 1075.98 985 1201.55 332.5 638.64 7.5 357.18 570"
-            fill="#fff"
-            stroke="#000"
-            stroke-miterlimit="10"
-        />
+        <g>
+            <g>
+                <polygon
+                    points="1201.9 333.4 1672.6 670 1076.6 1014.1 1201.9 333.4"
+                    fill="#fff"
+                    stroke-width="0"
+                />
+                <path
+                    d="m1202.3,334.2l469.4,335.7-594.4,343.2,125-678.9m-.7-1.7l-125.6,682.5,597.6-345-472-337.5h0Z"
+                    stroke-width="0"
+                />
+            </g>
+            <polygon
+                points="1210.3 907.6 1522 727.5 1522 587.5 1210.1 767.4 1210.3 907.6"
+                fill="#fff"
+                stroke="#000"
+                stroke-miterlimit="10"
+            />
+            <polygon
+                points="1147.9 723.5 1210.3 907.6 1210.1 767.4 1147.9 723.5"
+                fill="#fff"
+                stroke="#000"
+                stroke-miterlimit="10"
+            />
+            <polyline
+                points="1210.1 767.4 1522 587.5 1459.6 543.5 1147.9 723.5 1210.1 767.4"
+                fill="#fff"
+                stroke="#000"
+                stroke-miterlimit="10"
+            />
+            <g id="windows_north-2" data-name="windows_north">
+                <polygon
+                    points="1483.5 709.4 1440 734.3 1440 654.5 1483.5 629.7 1483.5 709.4"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1388.2 764.5 1344.7 789.3 1344.7 709.6 1388.2 684.7 1388.2 764.5"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1292.9 819.5 1249.4 844.4 1249.4 764.6 1292.9 739.7 1292.9 819.5"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1483.1 709.2 1504.8 696.7 1504.8 616.9 1483.1 629.4 1483.1 709.2"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1418.2 746.7 1439.8 734.2 1439.8 654.4 1418.2 666.9 1418.2 746.7"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1387.7 764.1 1409.4 751.6 1409.4 671.9 1387.7 684.4 1387.7 764.1"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1322.8 801.6 1344.4 789.1 1344.4 709.4 1322.8 721.9 1322.8 801.6"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1292.3 819.1 1314 806.6 1314 726.8 1292.3 739.3 1292.3 819.1"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+                <polygon
+                    points="1227.4 856.6 1249 844.1 1249 764.3 1227.4 776.8 1227.4 856.6"
+                    fill="#fff"
+                    stroke="#000"
+                    stroke-miterlimit="10"
+                />
+            </g>
+        </g>
     </g>
 </template>
